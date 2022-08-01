4821c42b604652e6e1dfb29ee0c2933f6501e404
Wan Lucas <wan.lucas.90@gmail.com>
2022-08-01 16:10:16 -0300

    :sparkles: updating Login component (2)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,85 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    loginDisabled: true,
+    isLoading: false,
+    name: '',
+    password: '',
+  };
+
+  rules = {
+    minNameLength: 3,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => this.handleButton());
+  };
+
+  handleButton = () => {
+    const { minNameLength } = this.rules;
+
+    this.setState(({ name }) => {
+      const someValueIsInvalid = name.length < minNameLength;
+
+      return { loginDisabled: someValueIsInvalid };
+    });
+  }
+
+  saveUser = (event) => {
+    const { name } = this.state;
+
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    createUser({ name }).then(() => this.login());
+  };
+
+  login = () => {
+    const { history } = this.props;
+
+    history.push('/search');
+  }
+
   render() {
+    const { loginDisabled, isLoading } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      isLoading ? (
+        <div> Carregando... </div>
+      ) : (
+        <div data-testid="page-login">
+          <form>
+            <input
+              type="text"
+              onChange={ this.handleChange }
+              name="name"
+              placeholder="Name"
+              data-testid="login-name-input"
+            />
+
+            <input
+              type="password"
+              name="password"
+              onChange={ this.handleChange }
+              placeholder="Password"
+            />
+
+            <input
+              type="submit"
+              value="Entrar"
+              disabled={ loginDisabled }
+              onClick={ this.saveUser }
+              data-testid="login-submit-button"
+            />
+          </form>
+        </div>
+      )
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.string,
+}.isRequired;
