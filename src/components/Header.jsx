4a963b89b0b92a119336839e53038fd9d639b03e
Wan Lucas <wan.lucas.90@gmail.com>
2022-08-01 16:59:26 -0300

    :sparkles: updating Header component  (3)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 export default class Header extends Component {
+  state = {
+    userName: '',
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
+  getUserName = async () => {
+    const { name } = await getUser();
+
+    this.setState({
+      userName: name,
+      isLoading: false,
+    });
+  }
+
   render() {
-    return (
-      <header data-testid="header-component"> Header </header>
+    const { userName, isLoading } = this.state;
+
+    return isLoading ? <div> Carregando... </div> : (
+      <header data-testid="header-component">
+        <span data-testid="header-user-name">{userName}</span>
+      </header>
     );
   }
 }
