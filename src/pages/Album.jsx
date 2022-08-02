d9b7ec202ee44f4b158015ea6d2ecca5b06116e3
Wan Lucas <wan.lucas.90@gmail.com>
2022-08-02 18:41:19 -0300

    :sparkles: creating initial Album structure

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  state = {
+    artistName: '',
+    collectionName: '',
+    songs: [],
+    isLoading: true,
+  }
+
+  componentDidMount = async () => {
+    const { match: { params: { id } } } = this.props;
+    const songs = await getMusics(id);
+    const { artistName, collectionName } = songs[0];
+
+    this.setState({
+      artistName,
+      collectionName,
+      songs: songs.slice(1),
+      isLoading: false,
+    });
+  }
+
   render() {
+    const {
+      songs,
+      artistName,
+      collectionName,
+      isLoading } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
+
+        {isLoading ? <span>Carregando...</span> : (
+          <div>
+            <h2 data-testid="artist-name">{artistName}</h2>
+            <p data-testid="album-name">{collectionName}</p>
+
+            <ul className="music-list">
+              {songs.map((song) => (
+                <MusicCard
+                  key={ song.trackName }
+                  song={ song }
+                  play={ this.play }
+                />
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  id: PropTypes.string,
+}.isRequired;
