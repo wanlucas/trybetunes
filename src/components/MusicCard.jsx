e38fb2903116ad3d7e51e6a50ff64029eb92bd8f
Wan Lucas <wan.lucas.90@gmail.com>
2022-08-02 19:40:53 -0300

    :sparkles: addint favorite feature to MusicCard

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Player from './Player';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
-  render() {
-    const { song } = this.props;
-    const { collectionName, trackName, artworkUrl100, previewUrl } = song;
+  state = {
+    checked: false,
+    isLoading: false,
+  }
 
-    return (
+  handleChange = async ({ target: { checked } }) => {
+    const { song } = this.props;
+
+    this.setState({
+      isLoading: true,
+      checked,
+    });
+
+    if (checked) await addSong(song);
+    else await removeSong(song);
+
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    const { checked, isLoading } = this.state;
+    const { song } = this.props;
+    const { collectionName, trackName, artworkUrl100, previewUrl, trackId } = song;
+
+    return isLoading ? <span>Carregando...</span> : (
       <li className="music_card">
         <div className="music_card-left">
           <input
@@ -19,7 +40,14 @@ export default class MusicCard extends Component {
         </div>
 
         <div className="music_card-right">
-          <button type="button" className="favorite-icon"> Favorite </button>
+          <input
+            type="checkbox"
+            className="favorite-icon"
+            value="Favorite"
+            checked={ checked }
+            onChange={ this.handleChange }
+            data-testid={ `checkbox-music-${trackId}` }
+          />
         </div>
 
         <Player src={ previewUrl } />
