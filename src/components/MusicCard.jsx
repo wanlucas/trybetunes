f5cd99ea588a7e13220b0831863e697846397079
Wan Lucas <wan.lucas.90@gmail.com>
2022-08-01 21:06:00 -0300

    :sparkles: adding MusicCard component

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export default class MusicCard extends Component {
+  render() {
+    const { album } = this.props;
+    const { collectionName, artworkUrl100, artistName, collectionId } = album;
+
+    return (
+      <Link
+        to={ `/album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+        className="music-card"
+      >
+
+        <img className="artist-image" src={ artworkUrl100 } alt={ collectionName } />
+        <div className="artist-info">
+          <h2>{ collectionName }</h2>
+          <p>{ artistName }</p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  album: PropTypes.object,
+  collectionName: PropTypes.string,
+  artworkUrl100: PropTypes.string,
+  artistName: PropTypes.string,
+}.isRequired;
